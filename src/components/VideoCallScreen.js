0f1ffe8d155c88d67701a90ec0bbfd31c8c860a9
Sooyeon Kim <supoo06138@gmail.com>
2025-06-03 01:11:16 +0900

    endCall 재수정

--- src/components/VideoCallScreen.js
+++ src/components/VideoCallScreen.js
@@ -3,6 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { useStompClient } from "../context/StompContext";
 
+
 const VideoCallScreen = () => {
   const navigate = useNavigate();
 
@@ -122,7 +123,6 @@ const VideoCallScreen = () => {
   const endCall = () => {
     sendSignalToPeer("end", receiverId || callerId);
     closePeerConnection();
-    navigate("/");
   };
 
   const startCamera = async () => {
@@ -298,7 +298,12 @@ const VideoCallScreen = () => {
           }}
         />
 
-        <IconButton src="/endcall.png" alt="통화 종료" onClick={endCall} />
+        <IconButton
+          src="/endcall.png"
+          alt="통화 종료"
+          onClick={
+            () => navigate("/")}
+        />
         <button onClick={createOffer}>통화 시작</button>
       </ButtonSide>
     </VideoContainer>
