40ba34bcc55ee0e937b3eb9dc57682bb750fc6df
comsieun <ja24z8@naver.com>
2025-06-11 00:55:34 +0900

    친구 삭제 및 eamil -> email 오타 수정

--- src/components/FriendProfilePopup.js
+++ src/components/FriendProfilePopup.js
@@ -23,7 +23,7 @@ function FriendProfilePopup({ friend, onClose }) {
     localStorage.setItem("receiverId", receiverId);
     navigate("/meeting", {
       state: {
-        callerId: localStorage.getItem("usereamil"), // 로그인한 사용자 ID
+        callerId: localStorage.getItem("useremail"), // 로그인한 사용자 ID
         receiverId: friend.email,
         incoming: false, // 전화를 거는 쪽이므로 false
       },
@@ -33,7 +33,7 @@ function FriendProfilePopup({ friend, onClose }) {
   const handleDeleteClick = async () => {
     try {
       const response = await axios.post(
-        `/api/friends/delete?requesterId=1`,
+        `/api/friends/delete?requesterId=${localStorage.getItem("userid")}`,
         {
           targetEmail: friend.email,
         },
